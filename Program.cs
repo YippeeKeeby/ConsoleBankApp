f90577c1929235304b5a8fded3ec37c7e11fb510
Jose <joseaw2003@gmail.com>
2026-06-01 16:06:42 -0400

    Progress before end of class

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ Main.main();
 public class Main{
     static List<Customer> customers = new List<Customer>();
     static String state = "";
+    static Admin admin = new Admin();
 
     public static void main()
     {
@@ -32,23 +33,21 @@ public class Main{
     {
         Console.WriteLine("Welcome to ABC Digital Bank!");
 
-        String result = login();
+        User result = login();
 
-        switch (result)
+       if (result is Admin){
+            adminDashboard();
+       }
+       else if (result is Customer)
         {
-            case "Admin":
-                adminDashboard();
-                break;
-            case "Customer":
-                customerDashboard();
-                break;
-            default:
-                Console.WriteLine(result);
-                break;
+            customerDashboard(result);
+        }
+        else{
+            Console.WriteLine("None");
         }
 
     }
-    private static String login()
+    private static User login()
     {
         Console.WriteLine("Please Enter username & password (seperate with a space)");
         //take in input
@@ -63,24 +62,19 @@ public class Main{
         String username = all.Substring(0, split_index);
         String password = all.Substring(split_index + 1);
 
-        return validate(username, password);
-    }
-
-    private static String validate(String username, String password)
-    {
         if (username.Equals("admin") && password.Equals("admin123")){
-            return "Admin";
+            return admin;
         }
 
         foreach (Customer customer in customers)
         {
             if (customer.verify(username, password))
             {
-                return "Customer";
+                return customer;
             }
         }
 
-        return "None";
+        return null;
     }
 
     private static void adminDashboard()
@@ -88,8 +82,46 @@ public class Main{
         Console.WriteLine("Not Admin implemented yet");
     }
 
-    private static void customerDashboard(){
-        Console.WriteLine("Not Customer implemented yet");
+    private static void customerDashboard(Customer c){
+        String choice = "";
+
+        while(choice != "7"){
+            Console.WriteLine("1) Create Account\n2) View All Accounts\n3) Deposit\n4) Withdraw\n5) Transfer\n6) Close Account\n7) Exit.");
+            choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("Will this be a checking or saving account? (c for checking, s for savings)");
+                    String which = Console.ReadLine();
+
+                    if (which == "c"){
+                        c.AddAccount("Checking");
+                    }
+                    else if (which == "s")
+                    {
+                        c.AddAccount("Saving");
+                    }
+
+                case "2":
+                    foreach (Account account in c.accounts){
+                        account.PrintAccount();
+                    }
+                case "3":
+                    Console.WriteLine("Where are you Depositing from?");
+
+                    Console.WriteLine("Where will you Deposit from?");
+                case "4":
+                    Console.WriteLine("Where are you Withdrawing from?");
+                case "5":
+                    Console.WriteLine("Where are you Transfering from?");
+                case "6":
+                    Console.WriteLine("Which account will you close?");
+            }
+        }
+        
+
+
     }
 
     private static void makeCustomers(int amount)
@@ -111,9 +143,14 @@ public class Main{
 #region Account
 abstract class Account
 {
+    protected int id;
     protected double balance;
     public abstract string AccountType { get; }
 
+    public Account(int id, double balance){
+        this.id = id;
+        this.balance = 0;
+    }
     public virtual void Deposit(double amount)
     {
         balance += amount;
@@ -122,12 +159,22 @@ abstract class Account
     public abstract void Withdraw(double amount);
 
     public abstract double AddInterest();
+
+    public virtual void PrintAccount()
+    {
+        Console.WriteLine($"{AccountType}\t|\t{id}\t|\t{balance}");
+    }
 }
 
 class CheckingAccount : Account
 {
     public override string AccountType => "CheckingAccount";
     private decimal overdraft_limit = 0.25m;
+
+    public CheckingAccount(int id){
+        this.id = id;
+        this.balance = 0;
+    }
     public override void Withdraw(double amount)
     {
         double new_balance = balance - amount;
@@ -153,6 +200,11 @@ class SavingsAccount : Account
     public override string AccountType => "SavingsAccount";
     private double interest_rate = 0.02;
 
+
+    public SavingsAccount(int id){
+        this.id = id;
+        this.balance = 0;
+    }
     public override void Withdraw(double amount)
     {
         double new_balance = balance - amount;
@@ -210,6 +262,54 @@ class Customer : User
     public bool verify(String username, String password)
     {
         return (this.username.Equals(username) && this.password.Equals(password));
+    }
+
+    public void AddAccount(String type)
+    {
+        Account new_account;
+
+        if (type == "Checking"){
+            new_account = new CheckingAccount(accounts.Count + 1);
+        }
+        else if (type == "Saving"){
+            new_account = new SavingsAccount(accounts.Count + 1);
+        }
+        else{
+            return;
+        }
+
+        this.accounts.Add(new_account);
+
+    }
+
+    private Account getAccount(int id){
+        foreach (Account account in this.accounts)
+        {
+            if (account.id == id){
+                return account;
+            }
+        }
+
+        return null;
+    }
+
+    public Account getCandidateAccounts(String action)
+    {
+        Console.WriteLine($"Where are you {action}ing from?");
+        int first_id = Console.ReadLine();
+        Account first = getAccount(Regex.Match(first_id, @"\d+").Value);
+        if (first == null){
+            return;
+        }
+
+        Console.WriteLine($"Where will you {action} to?");
+        int second_id = Console.ReadLine();
+        Account second = getAccount(Regex.Match(second_id, @"\d+").Value);
+        if (second == null){
+            return;
+        }
+
+        return [first, second];
     }
 }
 
--- obj/Debug/net10.0/Main.AssemblyInfo.cs
+++ obj/Debug/net10.0/Main.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e0a912ff378d64837a05b7a0a238586acdbb7dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
