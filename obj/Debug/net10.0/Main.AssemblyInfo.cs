1e76233e3a034f30ac8ad50b1bbbc81b95bbc955
Jose <joseaw2003@gmail.com>
2026-06-02 09:39:13 -0400

    a

--- obj/Debug/net10.0/Main.AssemblyInfo.cs
+++ obj/Debug/net10.0/Main.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f90577c1929235304b5a8fded3ec37c7e11fb510")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d33718b8b09aca6c1bf42d55d4fae7772a8b33c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
